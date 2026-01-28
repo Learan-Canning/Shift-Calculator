2d9738a05792866c02fe9450e63530c30cfa9e04
Learan-Canning <learancanning@mail.com>
2026-01-28 23:46:05 +0000

    add js scripts to make input work

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
-const hourlyPay = Number(document.getElementById("hourlyPay").value);
-const hoursPerShift = Number(document.getElementById("hoursPerShift").value);
-const shiftsPerMonth = Number(document.getElementById("shiftsPerMonth").value);
+// Get input values from the HTML form, and Calculate total monthly pay
+
+document.getElementById("calculateBtn").addEventListener("click", () => {
+
+    const hourlyPay = Number(document.getElementById("hourlyPay").value);
+    const hoursPerShift = Number(document.getElementById("hoursPerShift").value);
+    const shiftsPerMonth = Number(document.getElementById("shiftsPerMonth").value);
 
 
-const dailyPay = 
-    hourlyPay * hoursPerShift;
+    const dailyPay = 
+        hourlyPay * hoursPerShift;
 
-const totalPay = 
-    dailyPay * shiftsPerMonth;
+    const totalPay = 
+        dailyPay * shiftsPerMonth;
 
- 
+    document.getElementById("monthlyPay").textContent = `Monthly Pay: £${totalPay.toFixed(2)}`;
+
+});
